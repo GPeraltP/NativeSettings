b7b0e8226a890117e1fe7cd820a0e8c96931cdfb
GPeraltP <45854786+GPeraltP@users.noreply.github.com>
2019-03-29 10:32:27 -0500

    Update testios.java

--- src/android/testios.java
+++ src/android/testios.java
@@ -1,7 +1,12 @@
 package setting.config;
 
+import android.content.Intent;
+import android.content.Context;
+import android.provider.Settings;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
